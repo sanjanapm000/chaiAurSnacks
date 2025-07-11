daaf76e2a59c9c51d3052a565955c0466865a12c
sanjanapm000 <sanjanapm2002@gmail.com>
2025-07-11 13:02:48 +0530

    first commit

---
+++ chaiAurSnacks/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+   return (
+    <footer className="footer">
+        <p className="footer-text">&copy; {new Date().getFullYear()} chaiAurSnacks</p>
+    </footer>
+   )
+}
+
+export default Footer;
---
+++ chaiAurSnacks/src/Header.jsx
@@ -0,0 +1,16 @@
+
+function Header(){
+   return(
+    <header className="header">
+        <h1 className="title">chaiAurSnacks.</h1>
+        <ul className="list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Profile</a></li>
+        </ul>
+    </header>
+   )
+}
+
+export default Header;
